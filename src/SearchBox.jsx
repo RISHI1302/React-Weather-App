2cd3c379c98a17bcf31f7180d8fc96ce95228c96
RISHI1302 <rishi.ketan.panchal@gmail.com>
2026-05-31 14:29:59 +0530

    Used API to fetch data

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -8,15 +8,34 @@ export default function SearchBox() {
 
     let [city, setCity] = useState("");
 
+    const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+    const API_KEY = "67f7199ac12f020393760b37959c10a6";
+
+    let getWeatherInfo = async () => {
+        let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+        let jsonResponse = await response.json();
+        // console.log(jsonResponse);
+        let result = {
+            temp: jsonResponse.main.temp,
+            tempMin: jsonResponse.main.temp_min,
+            tempMax: jsonResponse.main.temp_max,
+            humidity: jsonResponse.main.humidity,
+            feelsLike: jsonResponse.main.feels_like,
+            weather: jsonResponse.weather[0].description,
+        };
+        console.log(result);
+    };
+
     let handleChange = (evt) => {
         setCity(evt.target.value);
-    }
+    };
 
     let handleSubmit = (evt) => {
         evt.preventDefault();
         console.log(city);
         setCity("");
-    }
+        getWeatherInfo();
+    };
 
     return (
         <div className="SearchBox">
